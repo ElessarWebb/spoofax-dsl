1529964aa51475762a65f66071469d13d9ab70c8
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-05-12 13:35:05 +0200

    Fixed macro test invocation

--- core/src/test/scala/TermMacroSpec.scala
+++ core/src/test/scala/TermMacroSpec.scala
@@ -3,8 +3,7 @@ import spoofax.scala.macros.Term
 
 class TermMacroSpec extends FlatSpec with Matchers {
 
-	/*"the macro" should "expand to return 42" {
-		println(Term.apply())
-		0
-	}*/
+	"the macro" should "expand to return 42" in {
+		Term() should be (42)
+	}
 }
